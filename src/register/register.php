fcaeebe9e1f15b02f5478fe0d4aeea81ae3304e6
juliatangerino <julianatangerino2017@gmail.com>
2022-04-03 23:49:30 -0300

    adding txt record

--- src/register/register.php
+++ src/register/register.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+$file= "register.txt";
+
+if(!file_exists($file)){
+    $register = fopen($file,"w");
+}else{
+    $register = fopen($file,"a");
+}
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){ 
     
     $name = $_POST["name"];
@@ -9,6 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nasc = $_POST["nasc"];
     $weight = $_POST["weight"];
     $code = $_POST["code"];
+    
     
     if(isset($name, $sex, $nasc, $weight, $code)) {
         $_SESSION['register'] = [
@@ -20,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             "code" => $code
         ];
 
-        $_SESSION['newRegister'] = true;
+        fwrite($register,"\n"."Nome:".$name."\n"."Tipo de pet:".$pet."\n"."Sexo do pet:".$sex."\n"."Data de nascimento:".$nasc."\n"."Peso:".$weight."\n"."Código da coleira:".$code."\n"."\n");
         
         header("location: ./registered.php");
     };
